43c96c989acdd32a523f63f2ae9fa33d8f6737f7
Zalutskyi <110344963+Zalutskyi@users.noreply.github.com>
2022-07-31 21:11:17 +0300

    Create sql.sql

---
+++ sql.sql
@@ -0,0 +1,24 @@
+USE [master];
+EXEC xp_instance_regwrite N'HKEY_LOCAL_MACHINE', 
+     N'Software\Microsoft\MSSQLServer\MSSQLServer',
+     N'LoginMode', REG_DWORD, 2;
+GO
+CREATE LOGIN Web 
+    WITH PASSWORD    = N'23062003Maks',
+    CHECK_POLICY     = ON,
+    CHECK_EXPIRATION = OFF;
+GO
+EXEC sp_addsrvrolemember 
+    @loginame = N'Web', 
+    @rolename = N'sysadmin';
+GO
+CREATE LOGIN PublicApi
+    WITH PASSWORD    = N'23062003Zal',
+    CHECK_POLICY     = ON,
+    CHECK_EXPIRATION = OFF;
+GO
+EXEC sp_addsrvrolemember 
+    @loginame = N'PublicApi', 
+    @rolename = N'sysadmin';
+GO
+exit
